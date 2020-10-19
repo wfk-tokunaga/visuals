eb3ce5e9c0a412c98a8eebb4297c6c711cd2ef3b
Will Tokunaga <thrill.ssb@gmail.com>
2020-10-19 15:26:56 -0700

    accidental pattern

--- visuals.js
+++ visuals.js
@@ -1,4 +1,5 @@
 let unit;
+let sideUnit;
 let width;
 let height;
 let ctx;
@@ -8,16 +9,26 @@ const colors = ['red', 'blue', 'green', 'orange'];
 
 window.onload = () => {
     let canvas = document.getElementById('pattern');
+
     if (canvas.getContext) {
         ctx = canvas.getContext("2d");
         [width, height] = [canvas.width, canvas.height];
-        unit = height / 10; //this lets us just do like 5*unit
+        unit = height / 20; //this lets us just do like 5*unit
+        sideUnit = unit * Math.sqrt(2) / 2;
         [originX, originY] = [width / 2, height / 2];
         ctx.translate(originX, originY); //moves origin :D
-        ctx.rotate(rad(90));
+        ctx.save(); //pushing onto the stack
+
+        // makeBigSquares();
+        // makeSmallSquares();
+        combinedDrawing();
+
+        //ctx.rotate(30);
+
+        ctx.translate(originX, originY); //moves origin :D
         ctx.save();
 
-        window.requestAnimationFrame(draw);
+        //window.requestAnimationFrame(draw);
 
     } else {
         console.log("Could not display pattern. Please try with a different browser.")
@@ -45,6 +56,153 @@ function draw(x) {
     }
     window.requestAnimationFrame(draw);
 }
+
+function combinedDrawing() {
+    // ctx.rotate(rad(45));
+    for (var i = 0; i < 4; i++) {
+        //loop through each ring of squares.
+        for (var ringNum = 1; ringNum < 10; ringNum++) {
+            //loop through each amount of squates in each ring
+            for (var squareNum = 0; squareNum < ringNum; squareNum++) {
+
+                //Make the small squares
+                var startX = unit*(ringNum + squareNum); 
+                let color = colors[i];
+                var startY = -unit*(2*ringNum - squareNum);
+                drawSmallSquare(startX, startY, color);
+
+                
+                ctx.rotate(rad(45));
+                var startX = unit*(-ringNum + 2*squareNum); 
+                var startY = -unit*(2*ringNum);
+                drawBigSquare(startX, startY);
+                //drawUpSpike(startX + unit, startY + 2*unit);
+            }
+        }
+        ctx.rotate(rad(90));
+    }
+}
+
+// function combinedDrawing() {
+//     // ctx.rotate(rad(45));
+//     for (var i = 0; i < 1; i++) {
+//         //loop through each ring of squares.
+//         for (var ringNum = 1; ringNum < 10; ringNum++) {
+//             //loop through each amount of squates in each ring
+//             for (var squareNum = 0; squareNum < ringNum; squareNum++) {
+
+//                 //Make the small squares
+//                 var startX = unit*(ringNum + squareNum); 
+//                 ctx.fillStyle = colors[i];
+//                 var startY = -unit*(2*ringNum - squareNum);
+//                 drawSmallSquare(startX, startY, color);
+
+
+
+//                 ctx.moveTo(startX + unit, startY);
+//                 ctx.lineTo(startX + 3*unit, startY - 2*unit);
+//                 ctx.stroke();
+
+//                 var startX = unit*(-ringNum + 2*squareNum); 
+//                 var startY = -unit*(2*ringNum);
+//                 drawBigSquare(startX, startY);
+//                 drawUpSpike(startX + unit, startY + 2*unit);
+//             }
+//         }
+//         ctx.rotate(rad(90));
+//     }
+// }
+
+function makeBigSquares() {
+    console.log(`Making Big Squares!`);
+
+    // loop through all the sides
+    for (var i = 0; i < 4; i++) {
+        //loop through each ring of squares.
+        for (var ringNum = 1; ringNum < 10; ringNum++) {
+            //loop through each amount of squates in each ring
+            for (var squareNum = 0; squareNum < ringNum; squareNum++) {
+                let startX = unit*(-ringNum + 2*squareNum); 
+                let startY = -unit*(2*ringNum);
+                console.log(`[startX, startY] = [${startX}, ${startY}]`);
+                drawBigSquare(startX, startY);
+                drawUpSpike(startX + unit, startY + 2*unit);
+            }
+        }
+        ctx.rotate(rad(90));
+    }
+}   
+
+function makeSmallSquares() {
+    console.log(`Making Small Squares!`);
+
+    // loop through all the sides
+    for (var i = 0; i < 4; i++) {
+        //loop through each ring of squares.
+        for (var ringNum = 1; ringNum < 10; ringNum++) {
+            //loop through each amount of squates in each ring
+            for (var squareNum = 0; squareNum < ringNum; squareNum++) {
+                let startX = unit*(ringNum + squareNum); 
+                let startY = -unit*(2*ringNum - squareNum);
+                console.log(`[startX, startY] = [${startX}, ${startY}]`);
+                ctx.strokeRect(startX, startY, unit, unit);
+                ctx.moveTo(startX + unit, startY);
+                ctx.lineTo(startX + 3*unit, startY - 2*unit);
+                ctx.stroke();
+            }
+        }
+        ctx.rotate(rad(90));
+    }
+}   
+
+//Draws a square opening upwards at starting x y
+let drawBigSquare = (startX, startY) => {
+    console.log(`Drawing Square at [${startX}, ${startY}]`);
+    ctx.beginPath();
+    //ctx.translate(startX, startY);
+
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(startX + unit, startY + unit);
+    ctx.lineTo(startX + 2*unit,  startY + 0);
+    ctx.lineTo(startX + unit, startY -unit);
+    ctx.lineTo(startX, startY);
+
+    ctx.stroke();
+}
+
+let drawUpSpike = (startX, startY) => {
+    ctx.beginPath();
+
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(startX - unit, startY - unit);
+    ctx.lineTo(startX - unit,  startY - 2*unit);
+    ctx.lineTo(startX, startY - unit);
+    ctx.lineTo(startX, startY);
+    ctx.lineTo(startX + unit, startY - unit);
+    ctx.lineTo(startX + unit,  startY - 2*unit);
+    ctx.lineTo(startX, startY - unit);
+
+    ctx.stroke();
+}
+
+let drawBigSquare2 = (sX, sY, color) => {
+    console.log(`Drawing Square at [${startX}, ${startY}]`);
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.fillRect(sX, sY, 2*sideUnit, 2*sideUnit);
+    ctx.stroke();
+}
+
+function drawSmallSquare(sX, sY, color) {
+    console.log(`Drawing Square at [${sX}, ${sY}]`);
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.fillRect(sX, sY, unit, unit);
+    ctx.stroke();
+}
+
+let rad = (degree) => degree * Math.PI / 180;
+
 // function draw(x) {
 //     ctx.restore();
 //     if (x % 10000 == 0) {
@@ -52,7 +210,6 @@ function draw(x) {
 //     }
 //     ctx.save();
     
-//     console.log(`x: ${x}`);
 //     for (var i = 0; i < 4; i++) {
 //         ctx.fillStyle = colors[i];
 //         ctx.fillRect(0,0,unit,unit);            
@@ -67,18 +224,3 @@ function draw(x) {
 //LOOK AT LATER
 // const takesCallback = (arg, num, num2) => arg(num, num2);
 // console.log(`${takesCallback((x, y) => x*y + 10, 100, 20) + 10}`);
-
-
-
-const patternLine = (ctx, newX, newY) => { ctx.lineTo() }
-
-//Draws a square opening upwards
-let drawSquare = (ctx, unit) => {
-    ctx.fillRect(0,0,unit,unit);
-}
-
-// function drawDiamond(ctx, unit) {
-//     ctx.fillRect(0,0,unit,unit);
-// }
-
-let rad = (degree) => degree * Math.PI / 180;
